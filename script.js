10975f6cc851ded3da72de847b25972e40b9becf
vitormartins <vitorrsilveriomartins@gmail.com>
2025-09-01 17:27:13 -0300

    falta alguns ajustes

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function toggleMenu() {
     document.getElementById("sidebar").classList.toggle("active");
 }
+ let currentIndex = 0;
+                        const images = document.querySelectorAll('.carousel-img');
+
+                        function showImage(index) {
+                            images.forEach((img, i) => {
+                                img.classList.toggle('active', i === index);
+                            });
+                        }
+
+                        function moveCarousel(direction) {
+                            currentIndex = (currentIndex + direction + images.length) % images.length;
+                            showImage(currentIndex);
+                        }
+
+                        showImage(currentIndex);
